53ee5cb8092e3470f699d658f93396597331c86d
Yash Diwaker <190104076@hbtu.ac.in>
2023-01-22 04:04:47 +0530

    api integration done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Card from './component/Card';
 import CardModal from './component/CardModal';
@@ -7,18 +7,24 @@ import WarningCard from './component/WarningCard';
 import Unmarked from './component/Unmarked';
 import { BrowserRouter, Route ,Routes} from 'react-router-dom';
 import Footer from './component/Footer';
+import axios from 'axios';
 
 function App() {
-  const [open, setOpen] = React.useState(false);
+  const [portFolio,setPortfolio]=React.useState(150000)
+  const [data,setdata]=useState([]);
+  useEffect(()=>{
+             axios.get("https://mocki.io/v1/61a36370-212d-4ac7-baf6-355a14262cb7").then((data)=>{
+              setdata(data.data.portfolio)
+             })
+             
 
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
+  },[])
   return (
     <div style={{position:"relative"}}>
     <BrowserRouter>
     <Routes>
-    <Route exact path='/' element={<Card />}></Route>
-    <Route exact path='/unmarked' element={<Unmarked />}></Route>
+    <Route exact path='/' element={<Card  portFolio={portFolio} setPortfolio={setPortfolio} data={data}/>}></Route>
+    <Route exact path='/unmarked' element={<Unmarked  portFolio={portFolio} setPortfolio={setPortfolio} data={data}/>}></Route>
      </Routes>
      {/* <Footer></Footer> */}
      </BrowserRouter>
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -4,10 +4,7 @@ import { FaBell } from "react-icons/fa";
 import "./Card.css";
 import { Link } from "react-router-dom";
 
-
-
 const Card = (props) => {
-    
   return (
     <>
       <div className="Container">
@@ -16,12 +13,19 @@ const Card = (props) => {
             <span> WELCOME</span>
             <span style={{ fontWeight: "bold", marginLeft: "1rem" }}>Yash</span>
           </div>
-          <div className="logo"><span><FaBell size={"1.5em"}  style={{borderColor:"1.5px solid rgba(85, 22, 170, 1)"}}/></span></div>
+          <div className="logo">
+            <span>
+              <FaBell
+                size={"1.5em"}
+                style={{ borderColor: "1.5px solid rgba(85, 22, 170, 1)" }}
+              />
+            </span>
+          </div>
         </div>
         <div className="portfolio">
           <div>
             <h2>Total Portfolio Value</h2>
-            <h2> $ 150000</h2>
+            <h2> $ {props.portFolio}</h2>
           </div>
         </div>
         <div className="Second_section">
@@ -41,77 +45,62 @@ const Card = (props) => {
         <div className="fourth_section">
           <div className="fourth_section-1">
             <h1>Marked Mutual Funds</h1>
-            
           </div>
           <div>
-            <Link to="./Unmarked"> <button className="fourth_section_button" onClick={props.onOpenModal}>Unmark funds</button></Link>
-           
+            <Link to="./Unmarked">
+              {" "}
+              <button className="fourth_section_button">Unmark funds</button>
+            </Link>
           </div>
         </div>
-        <div className="main_card">
-            <h3>ICICI Small Cap- Direct Plan</h3>
-            <div className="main_card-child">
-                <span style={{color:" rgba(129, 129, 165, 1)"}}> <h5>Current value</h5></span>
-                <span>$480</span>
-                <span  style={{color:" rgba(129, 129, 165, 1)"}}><h5>Units</h5></span>
-                <span><h3>54</h3></span>
+        {props.data.map((index) => {
+          return (
+            <div className="main_card">
+              <h3>{index.scheme_name}</h3>
+              <div className="main_card_child">
+                <div className="main_card-child-1">
+                  <div className="main_card-child-1-1">
+                    <div style={{ color: " rgba(129, 129, 165, 1)" }}>
+                      {" "}
+                      <h5>Current value</h5>
+                    </div>
+                    <div>
+                      <h5>${index.current_value.toString().slice(0, 3)}</h5>
+                    </div>
+                  </div>
+                  <div className="main_card-child-1-1">
+                    <div style={{ color: " rgba(129, 129, 165, 1)" }}>
+                      <h5>Folio Number</h5>
+                    </div>
+                    <div>
+                      <h5>${index.folio_number.toString().slice(0, 4)}</h5>
+                    </div>
+                  </div>
+                </div>
+                <div className="main_card-child-2">
+                  <div className="main_card-child-1-1">
+                    <div style={{ color: " rgba(129, 129, 165, 1)" }}>
+                      <h5>Units</h5>
+                    </div>
+                    <div>
+                      <h5>{index.units}</h5>
+                    </div>
+                  </div>
+                  <div className="main_card-child-1-1">
+                    <div style={{ color: " rgba(129, 129, 165, 1)" }}>
+                      <h5>Nav</h5>
+                    </div>
+                    <div>
+                      <h5>{index.nav.toString().slice(0, 3)}</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="main_card-child">
-                <span style={{color:" rgba(129, 129, 165, 1)"}}> <h5>Folio Number</h5></span>
-                <span>$480</span>
-                <span  style={{color:" rgba(129, 129, 165, 1)"}}><h5>Nav</h5></span>
-                <span><h3>54</h3></span>
-            </div>
-        </div>
-        <div className="main_card">
-            <h3>ICICI Small Cap- Direct Plan</h3>
-            <div className="main_card-child">
-                <span style={{color:" rgba(129, 129, 165, 1)"}}> <h5>Current value</h5></span>
-                <span>$480</span>
-                <span  style={{color:" rgba(129, 129, 165, 1)"}}><h5>Units</h5></span>
-                <span><h3>54</h3></span>
-            </div>
-            <div className="main_card-child">
-                <span style={{color:" rgba(129, 129, 165, 1)"}}> <h5>Folio Number</h5></span>
-                <span>$480</span>
-                <span  style={{color:" rgba(129, 129, 165, 1)"}}><h5>Nav</h5></span>
-                <span><h3>54</h3></span>
-            </div>
-        </div>
-        <div className="main_card">
-            <h3>ICICI Small Cap- Direct Plan</h3>
-            <div className="main_card-child">
-                <span style={{color:" rgba(129, 129, 165, 1)"}}> <h5>Current value</h5></span>
-                <span>$480</span>
-                <span  style={{color:" rgba(129, 129, 165, 1)"}}><h5>Units</h5></span>
-                <span><h3>54</h3></span>
-            </div>
-            <div className="main_card-child">
-                <span style={{color:" rgba(129, 129, 165, 1)"}}> <h5>Folio Number</h5></span>
-                <span>$480</span>
-                <span  style={{color:" rgba(129, 129, 165, 1)"}}><h5>Nav</h5></span>
-                <span><h3>54</h3></span>
-            </div>
-        </div>
-        <div className="main_card">
-            <h3>ICICI Small Cap- Direct Plan</h3>
-            <div className="main_card-child">
-                <span style={{color:" rgba(129, 129, 165, 1)"}}> <h5>Current value</h5></span>
-                <span>$480</span>
-                <span  style={{color:" rgba(129, 129, 165, 1)"}}><h5>Units</h5></span>
-                <span><h3>54</h3></span>
-            </div>
-            <div className="main_card-child">
-                <span style={{color:" rgba(129, 129, 165, 1)"}}> <h5>Folio Number</h5></span>
-                <span>$480</span>
-                <span  style={{color:" rgba(129, 129, 165, 1)"}}><h5>Nav</h5></span>
-                <span><h3>54</h3></span>
-            </div>
-        </div>
-       
+          );
+        })}
       </div>
       <Footer></Footer>
-
     </>
   );
 };
